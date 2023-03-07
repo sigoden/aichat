e36efd3301ac1c95efdb4cf1f362be29b8ece27f
sigoden <sigoden@gmail.com>
2023-03-07 10:38:05 +0800

    simulate typing effect.

--- src/render/markdown.rs
+++ src/render/markdown.rs
@@ -9,8 +9,9 @@ pub struct MarkdownRender {
     syntax_set: SyntaxSet,
     theme: Theme,
     md_syntax: SyntaxReference,
-    code_syntax: Option<SyntaxReference>,
-    code_block: bool,
+    txt_syntax: SyntaxReference,
+    code_syntax: SyntaxReference,
+    line_type: LineType,
 }
 
 impl MarkdownRender {
@@ -18,12 +19,16 @@ impl MarkdownRender {
         let syntax_set = SyntaxSet::load_defaults_newlines();
         let theme: Theme = serde_yaml::from_slice(THEME).unwrap();
         let md_syntax = syntax_set.find_syntax_by_extension("md").unwrap().clone();
+        let txt_syntax = syntax_set.find_syntax_by_extension("txt").unwrap().clone();
+        let code_syntax = txt_syntax.clone();
+        let line_type = LineType::Normal;
         Self {
             syntax_set,
             theme,
             md_syntax,
-            code_syntax: None,
-            code_block: false,
+            code_syntax,
+            txt_syntax,
+            line_type,
         }
     }
 
@@ -36,23 +41,52 @@ impl MarkdownRender {
 
     pub fn render_line(&mut self, line: &str) -> Option<String> {
         if let Some(lang) = detect_code_block(line) {
-            if self.code_block {
-                self.code_block = false;
-                self.code_syntax = None;
-            } else {
-                self.code_block = true;
-                if !lang.is_empty() {
-                    self.code_syntax = self.find_syntax(&lang).cloned();
+            match self.line_type {
+                LineType::Normal | LineType::CodeEnd => {
+                    self.line_type = LineType::CodeBegin;
+                    self.code_syntax = if lang.is_empty() {
+                        self.txt_syntax.clone()
+                    } else {
+                        self.find_syntax(&lang)
+                            .cloned()
+                            .unwrap_or_else(|| self.txt_syntax.clone())
+                    };
+                }
+                LineType::CodeBegin | LineType::CodeInner => {
+                    self.line_type = LineType::CodeEnd;
+                    self.code_syntax = self.txt_syntax.clone();
                 }
             }
             self.render_line_inner(line, &self.md_syntax)
-        } else if self.code_block {
-            self.code_syntax
-                .as_ref()
-                .and_then(|syntax| self.render_line_inner(line, syntax))
         } else {
-            self.render_line_inner(line, &self.md_syntax)
+            match self.line_type {
+                LineType::Normal => self.render_line_inner(line, &self.md_syntax),
+                LineType::CodeEnd => {
+                    self.line_type = LineType::Normal;
+                    self.render_line_inner(line, &self.md_syntax)
+                }
+                LineType::CodeBegin => {
+                    self.line_type = LineType::CodeInner;
+                    self.render_line_inner(line, &self.code_syntax)
+                }
+                LineType::CodeInner => self.render_line_inner(line, &self.code_syntax),
+            }
         }
+    }
+
+    pub fn render_line_stateless(&self, line: &str) -> String {
+        let output = if detect_code_block(line).is_some() {
+            self.render_line_inner(line, &self.md_syntax)
+        } else {
+            match self.line_type {
+                LineType::Normal | LineType::CodeEnd => {
+                    self.render_line_inner(line, &self.md_syntax)
+                }
+                _ => self.render_line_inner(line, &self.code_syntax),
+            }
+        };
+
+        output.unwrap_or_else(|| line.to_string())
     }
 
     fn render_line_inner(&self, line: &str, syntax: &SyntaxReference) -> Option<String> {
@@ -69,6 +103,14 @@ impl MarkdownRender {
                 .and_then(|(_, ext)| self.syntax_set.find_syntax_by_extension(ext))
         })
     }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum LineType {
+    Normal,
+    CodeBegin,
+    CodeInner,
+    CodeEnd,
 }
 
 const LANGEGUATE_NAME_EXTS: [(&str, &str); 21] = [
