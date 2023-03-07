c7fcdb174440817706b818154a6bbcaf58948caa
sigoden <sigoden@gmail.com>
2023-03-07 19:04:54 +0800

    refactor: adjust repl (#30)
    - abandon .copy
    - rename .multiple => .editor
    - if command successed, do not output `Done`
    - if command failed, output `Error: <error message>`

--- src/utils.rs
+++ src/utils.rs
@@ -1,17 +1,9 @@
-use anyhow::{anyhow, Result};
 use chrono::prelude::*;
-use copypasta::{ClipboardContext, ClipboardProvider};
 use std::io::{stdout, Write};
 
 pub fn dump<T: ToString>(text: T, newlines: usize) {
     print!("{}{}", text.to_string(), "\n".repeat(newlines));
     let _ = stdout().flush();
-}
-
-pub fn copy(src: &str) -> Result<()> {
-    ClipboardContext::new()
-        .and_then(|mut ctx| ctx.set_contents(src.to_string()))
-        .map_err(|err| anyhow!("Failed to copy, {err}"))
 }
 
 pub fn now() -> String {
