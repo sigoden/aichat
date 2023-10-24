a8b4ddb70738e03aaa4d3e8466c0440ac42018b3
sigoden <sigoden@gmail.com>
2023-10-24 17:38:25 +0800

    feat: use backslash at end of line for multiline text

--- src/repl/validator.rs
+++ src/repl/validator.rs
@@ -6,55 +6,10 @@ pub struct ReplValidator;
 
 impl Validator for ReplValidator {
     fn validate(&self, line: &str) -> ValidationResult {
-        if incomplete_brackets(line) {
+        if line.ends_with('\\') {
             ValidationResult::Incomplete
         } else {
             ValidationResult::Complete
         }
-    }
-}
-
-fn incomplete_brackets(line: &str) -> bool {
-    let mut balance: Vec<char> = Vec::new();
-    let mut symbol = None;
-    for c in line.chars() {
-        match symbol {
-            Some(s) => match (s, c) {
-                ('{', '}') | ('(', ')') => {
-                    balance.pop();
-                }
-                _ if s == c => {
-                    balance.push(c);
-                }
-                _ => {}
-            },
-            None => match c {
-                '{' | '(' => {
-                    balance.push(c);
-                    symbol = Some(c);
-                }
-                _ => {}
-            },
-        }
-    }
-
-    !balance.is_empty()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_incomplete_brackets() {
-        assert!(incomplete_brackets("{"));
-        assert!(incomplete_brackets("("));
-        assert!(!incomplete_brackets("{}"));
-        assert!(!incomplete_brackets("()"));
-        assert!(!incomplete_brackets("{ab\nc}"));
-        assert!(!incomplete_brackets("(ab\nc)"));
-        assert!(!incomplete_brackets("{[}"));
-        assert!(!incomplete_brackets("{{{{{}}}}}"));
-        assert!(incomplete_brackets("{{}"));
     }
 }
