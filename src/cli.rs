632a6fcc0bfb5b33858cd0fa3876e43ce2073cb3
sigoden <sigoden@gmail.com>
2023-10-29 17:36:17 +0800

    feat: add `--light-theme` option (#169)

--- src/cli.rs
+++ src/cli.rs
@@ -4,39 +4,42 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Cli {
-    /// Choose a model
+    /// List all models
+    #[clap(long)]
+    pub list_models: bool,
+    /// Choose a llm model
     #[clap(short, long)]
     pub model: Option<String>,
+    /// List all roles
+    #[clap(long)]
+    pub list_roles: bool,
+    /// Select a role
+    #[clap(short, long)]
+    pub role: Option<String>,
     /// Add a GPT prompt
     #[clap(short, long)]
     pub prompt: Option<String>,
+    /// List sessions
+    #[clap(long)]
+    pub list_sessions: bool,
+    /// Initiate or continue a session
+    #[clap(short = 's', long)]
+    pub session: Option<Option<String>>,
+    /// Print information
+    #[clap(long)]
+    pub info: bool,
+    /// Use light theme
+    #[clap(long)]
+    pub light_theme: bool,
     /// Disable syntax highlighting
     #[clap(short = 'H', long)]
     pub no_highlight: bool,
     /// No stream output
     #[clap(short = 'S', long)]
     pub no_stream: bool,
-    /// List all roles
-    #[clap(long)]
-    pub list_roles: bool,
-    /// List all models
-    #[clap(long)]
-    pub list_models: bool,
-    /// Select a role
-    #[clap(short, long)]
-    pub role: Option<String>,
-    /// Print system-wide information
-    #[clap(long)]
-    pub info: bool,
     /// Run in dry run mode
     #[clap(long)]
     pub dry_run: bool,
-    /// List sessions
-    #[clap(long)]
-    pub list_sessions: bool,
-    /// Initiate or continue named session
-    #[clap(short = 's', long)]
-    pub session: Option<Option<String>>,
     /// Input text
     text: Vec<String>,
 }
