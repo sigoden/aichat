667aec22c223ad8367de3e86cb64d4e41c064dad
sigoden <sigoden@gmail.com>
2023-11-07 16:09:05 +0800

    update clipboard

--- src/utils/clipboard.rs
+++ src/utils/clipboard.rs
@@ -1,11 +1,7 @@
-use lazy_static::lazy_static;
-use std::sync::Arc;
-use std::sync::Mutex;
-
 #[cfg(not(any(target_os = "android", target_os = "emscripten")))]
-lazy_static! {
-    static ref CLIPBOARD: Arc<Mutex<Option<arboard::Clipboard>>> =
-        Arc::new(Mutex::new(arboard::Clipboard::new().ok()));
+lazy_static::lazy_static! {
+    static ref CLIPBOARD: std::sync::Arc<std::sync::Mutex<Option<arboard::Clipboard>>> =
+        std::sync::Arc::new(std::sync::Mutex::new(arboard::Clipboard::new().ok()));
 }
 
 #[cfg(not(any(target_os = "android", target_os = "emscripten")))]
@@ -19,6 +15,6 @@ pub fn set_text(text: &str) -> anyhow::Result<()> {
 }
 
 #[cfg(any(target_os = "android", target_os = "emscripten"))]
-pub fn set_text(text: &str) -> anyhow::Result<()> {
+pub fn set_text(_text: &str) -> anyhow::Result<()> {
     anyhow::bail!("No available clipboard")
 }
