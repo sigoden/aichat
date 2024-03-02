c01da928e1dab3c627f20c216c7e84361c713b29
sigoden <sigoden@gmail.com>
2024-03-02 13:03:08 +0000

    update code role prompt

--- src/config/role.rs
+++ src/config/role.rs
@@ -57,8 +57,9 @@ APPLY MARKDOWN formatting when possible."#
     pub fn for_code() -> Self {
         Self {
             name: "__code__".into(),
-            prompt: r#"Provide only code in plain text format without any further description or formatting.
-Exclude symbols like ```.
+            prompt: r#"Provide only code as output without any description.
+Provide only code in plain text format without Markdown formatting.
+Do not include symbols such as ``` or ```python.
 If there is a lack of details, provide most logical solution.
 You are not allowed to ask for more details.
 For example if the prompt is "Hello world Python", you should return "print('Hello world')"."#
