855fd219c39c3a304d15c4d2061d446215685928
sigoden <sigoden@gmail.com>
2025-02-14 22:20:16 +0800

    feat: remove supports for hyperbolic and novita

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -33,7 +33,7 @@ register_client!(
     (bedrock, "bedrock", BedrockConfig, BedrockClient),
 );
 
-pub const OPENAI_COMPATIBLE_PROVIDERS: [(&str, &str); 24] = [
+pub const OPENAI_COMPATIBLE_PROVIDERS: [(&str, &str); 22] = [
     ("ai21", "https://api.ai21.com/studio/v1"),
     (
         "cloudflare",
@@ -46,12 +46,10 @@ pub const OPENAI_COMPATIBLE_PROVIDERS: [(&str, &str); 24] = [
     ("github", "https://models.inference.ai.azure.com"),
     ("groq", "https://api.groq.com/openai/v1"),
     ("hunyuan", "https://api.hunyuan.cloud.tencent.com/v1"),
-    ("hyperbolic", "https://api.hyperbolic.xyz/v1"),
     ("lingyiwanwu", "https://api.lingyiwanwu.com/v1"),
     ("minimax", "https://api.minimax.chat/v1"),
     ("mistral", "https://api.mistral.ai/v1"),
     ("moonshot", "https://api.moonshot.cn/v1"),
-    ("novita", "https://api.novita.ai/v3/openai"),
     ("openrouter", "https://openrouter.ai/api/v1"),
     ("perplexity", "https://api.perplexity.ai"),
     (
